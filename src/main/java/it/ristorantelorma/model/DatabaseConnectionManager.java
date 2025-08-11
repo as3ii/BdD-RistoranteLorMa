14224a80d5b701069b2fc2a544dcb29e90980a98
marco zignani <marco.zignani@studio.unibo.it>
2025-08-11 17:57:06 +0200

    Enabled opening of FirstPage, fixed string format in DatabaseConnectionManager

--- src/main/java/it/ristorantelorma/model/DatabaseConnectionManager.java
+++ src/main/java/it/ristorantelorma/model/DatabaseConnectionManager.java
@@ -44,7 +44,7 @@ public final class DatabaseConnectionManager {
         final String user = env.getOrDefault("DB_USER", DEFAULT_USER);
         final String password = env.getOrDefault("DB_PASSWORD", DEFAULT_PASSWORD);
 
-        final String url = String.format("jdbc:mysql://%s:%i/%s", hostname, port, dbName);
+        final String url = String.format("jdbc:mysql://%s:%d/%s", hostname, port, dbName);
 
         try {
             logger.log(Level.INFO, "Connecting to " + url);
