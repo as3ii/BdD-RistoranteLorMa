42cb7c0db71f1bb1c974673e684173d8dea1878c
as3ii <as3ii777@gmail.com>
2025-08-23 19:24:38 +0200

    Upgraded shadow plugin from 8.3.6 to 9.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     java
     application
     id("org.danilopianini.gradle-java-qa") version "1.87.0"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "9.0.2"
 }
 
 dependencies {
